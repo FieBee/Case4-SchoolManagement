0d057419224220c794d1be1aaba3c0113f5833c1
binhnguyen95 <invalidmyarse@gmail.com>
2022-10-03 16:25:10 +0700

    done teacher controller

--- src/main/java/com/example/demo/repository/TeacherRepo.java
+++ src/main/java/com/example/demo/repository/TeacherRepo.java
@@ -2,9 +2,11 @@ package com.example.demo.repository;
 
 import com.example.demo.model.entity.Teacher;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface TeacherRepo  extends PagingAndSortingRepository<Teacher, Long> {
     Teacher findTeacherById(Long id);
     Optional<Teacher> findByLastName(String name);
