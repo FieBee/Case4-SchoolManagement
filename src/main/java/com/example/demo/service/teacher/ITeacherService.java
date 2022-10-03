da109f6f8fbc515d600bf6c2ce1b759bb4ce8ac3
binhnguyen95 <invalidmyarse@gmail.com>
2022-10-03 15:57:03 +0700

    add teacher service

---
+++ src/main/java/com/example/demo/service/teacher/ITeacherService.java
@@ -0,0 +1,7 @@
+package com.example.demo.service.teacher;
+
+import com.example.demo.model.entity.Teacher;
+import com.example.demo.service.GeneralService;
+
+public interface ITeacherService  extends GeneralService<Teacher> {
+}
