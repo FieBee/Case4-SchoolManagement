68583fec8d4fa43dc0eea08870812b64998c1f0e
binhnguyen95 <invalidmyarse@gmail.com>
2022-10-03 14:33:37 +0700

    add teacher

--- src/main/java/com/example/demo/controller/TeacherController.java
+++ src/main/java/com/example/demo/controller/TeacherController.java
@@ -1,2 +1,17 @@
-package com.example.demo.controller;public class TeacherController {
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("/teacher")
+public class TeacherController {
+
+    @GetMapping()
+    public ModelAndView getUser(){
+        return new ModelAndView("teacher");
+    }
 }
