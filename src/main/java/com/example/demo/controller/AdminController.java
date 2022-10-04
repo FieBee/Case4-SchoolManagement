16730b9ea9f4c4b3ef800e754aca3db8d224471e
binhnguyen95 <invalidmyarse@gmail.com>
2022-10-04 14:52:07 +0700

    conflict

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -10,6 +10,6 @@ import org.springframework.web.servlet.ModelAndView;
 public class AdminController {
     @GetMapping
     public ModelAndView homeAdmin(){
-        return new ModelAndView("/test/admin");
+        return new ModelAndView("index");
     }
 }
