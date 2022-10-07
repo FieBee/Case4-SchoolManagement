123f85a52d2b7452ca43e80730519dbe88df94c4
binhnguyen95 <invalidmyarse@gmail.com>
2022-10-07 14:34:19 +0700

    add account controller, edit socu config

---
+++ src/main/java/com/example/demo/controller/AccountController.java
@@ -0,0 +1,67 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.entity.Account;
+import com.example.demo.service.account.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/account")
+public class AccountController {
+    @Autowired
+    AccountService accountService;
+
+    @GetMapping
+    public ResponseEntity<Iterable<Account>> findAllAccount(){
+        List<Account> accounts = (List<Account>)accountService.findAll();
+        if (accounts.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(accounts,HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Account> findById(@PathVariable Long id){
+        Optional<Account> accounts =accountService.findById(id);
+        return accounts.map(account -> new ResponseEntity<>(account, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<Account> save(@RequestBody Account account) {
+        return new ResponseEntity<>(accountService.save(account), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Account> updateAccount(@PathVariable Long id, @RequestBody Account account) {
+        Optional<Account> accounts = accountService.findById(id);
+        if (!accounts.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        account.setId(accounts.get().getId());
+        return new ResponseEntity<>(accountService.save(account),HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Account> deleteAccount(@PathVariable Long id){
+        Optional<Account> accounts = accountService.findById(id);
+        if (!accounts.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        accountService.remove(id);
+        return new ResponseEntity<>(accounts.get(),HttpStatus.OK);
+    }
+
+    @GetMapping("/list")
+    public ModelAndView getAllAccount() {
+        ModelAndView modelAndView = new ModelAndView("/ajaxTeacher");
+        modelAndView.addObject("accounts", accountService.findAll());
+        return modelAndView;
+    }
+}
